0a973b046a1eeef6aff7255298ba0c215780277a
Dan Kingo <danieldejen23@gmail.com>
2024-05-28 20:34:19 +0300

    add correct answer functionality

---
+++ src/assets/questions.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    question: "What is the capital of France?",
+    option1: "Berlin",
+    option2: "Madrid",
+    option3: "Paris",
+    option4: "Rome",
+    correctAnswer: 3,
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    option1: "Earth",
+    option2: "Mars",
+    option3: "Jupiter",
+    option4: "Saturn",
+    correctAnswer: 2,
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    option1: "Atlantic Ocean",
+    option2: "Indian Ocean",
+    option3: "Arctic Ocean",
+    option4: "Pacific Ocean",
+    correctAnswer: 4,
+  },
+  {
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    option1: "Harper Lee",
+    option2: "Mark Twain",
+    option3: "Ernest Hemingway",
+    option4: "F. Scott Fitzgerald",
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the smallest unit of life?",
+    option1: "Atom",
+    option2: "Molecule",
+    option3: "Cell",
+    option4: "Organ",
+    correctAnswer: 3,
+  },
+];
+
+export default data;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,16 +1,48 @@
 import "./Quiz.css";
-
+import data from "../../assets/questions";
+import { useRef, useState } from "react";
 const Quiz = () => {
+  const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(data[index]);
+  const [disable, setDisable] = useState(false);
+  const Option1 = useRef(null);
+  const Option2 = useRef(null);
+  const Option3 = useRef(null);
+  const Option4 = useRef(null);
+
+  const optionRef = [Option1, Option2, Option3, Option4];
+  const checkAnswer = (e, answer) => {
+    if (disable === false) {
+      if (question.correctAnswer === answer) {
+        e.target.classList.add("correct");
+        setDisable(true);
+      } else {
+        e.target.classList.add("wrong");
+        setDisable(true);
+        optionRef[question.correctAnswer - 1].current.classList.add("correct");
+      }
+    }
+  };
   return (
     <div className="container">
       <h1>Quiz App</h1>
 
-      <p>1. Which device is required for the internet connection?</p>
+      <p>
+        {index + 1}. {question.question}
+      </p>
       <ul>
-        <li>Modem</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
+        <li onClick={(e) => checkAnswer(e, 1)} ref={Option1}>
+          {question.option1}
+        </li>
+        <li onClick={(e) => checkAnswer(e, 2)} ref={Option2}>
+          {question.option2}
+        </li>
+        <li onClick={(e) => checkAnswer(e, 3)} ref={Option3}>
+          {question.option3}
+        </li>
+        <li onClick={(e) => checkAnswer(e, 4)} ref={Option4}>
+          {question.option4}
+        </li>
       </ul>
       <div className="footer">
         <button>Next</button>
